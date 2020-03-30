c0bb705f1fc5f3fed995ae2b8d952d800a8d009a
Gyakusetsu <reymarbc@gmail.com>
2020-03-30 10:48:53 +0800

    Return to original

--- index.js
+++ index.js
@@ -14,13 +14,7 @@ const { textHandler } = require('./handler/text');
 
 app.use(bodyParser.json());
 
-app.post('/slack/events', (req, res) => {
-  console.log(req.body);
-  res.status(200).json({
-    "challenge": req.body.challenge
-  });
-})
-// app.use('/slack/events', slackEvents.expressMiddleware());
+app.use('/slack/events', slackEvents.expressMiddleware());
 
 slackEvents.on('message', (event)=> {
   console.log(event);
